50f809e12340820c6597d89c7094043d67c7d763
kevinmerckx <kevin.merckx.enst@gmail.com>
2015-01-30 15:28:10 +0100

    .updates()

--- src/conditions.js
+++ src/conditions.js
@@ -1,67 +1,73 @@
 'use strict';
 
 var conditions = function(when, names, callback) {
-    if(names.length === 1) {
-        var name = names[0];
-        return {
+	if(names.length === 1) {
+		var name = names[0];
+		return {
 			remove: function () {
 				when.remove();
 			},
-            verifies: function(f) {
-                callback(function(newValues,oldValues){
-                    return f(newValues[name],oldValues[name]);
-                });
-                return actions(when);
-            },
-            is: function(v) {
-                callback(function(newValues,oldValues){
-                    return newValues[name] === v;
-                });
-                return actions(when);
-            },
-            isNot: function(v) {
-                callback(function(newValues,oldValues){
-                    return newValues[name] !== v;
-                });
-                return actions(when);
-            },
-            isDefined: function() {
-                callback(function(newValues,oldValues){
-                    return newValues[name] !== undefined;
-                });
-                return actions(when);
-            },
-            isUndefined: function() {
-                callback(function(newValues,oldValues){
-                    return newValues[name] === undefined;
-                });
-                return actions(when);
-            },
-            becomes: function(v) {
-                callback(function(newValues,oldValues){
-                    return newValues[name] === v && newValues[name] !== oldValues[name];
-                });
-                return actions(when);
-            }
-        };
-    } else {
-        return {
+			verifies: function(f) {
+				callback(function(newValues,oldValues){
+					return f(newValues[name],oldValues[name]);
+				});
+				return actions(when);
+			},
+			is: function(v) {
+				callback(function(newValues,oldValues){
+					return newValues[name] === v;
+				});
+				return actions(when);
+			},
+			isNot: function(v) {
+				callback(function(newValues,oldValues){
+					return newValues[name] !== v;
+				});
+				return actions(when);
+			},
+			isDefined: function() {
+				callback(function(newValues,oldValues){
+					return newValues[name] !== undefined;
+				});
+				return actions(when);
+			},
+			isUndefined: function() {
+				callback(function(newValues,oldValues){
+					return newValues[name] === undefined;
+				});
+				return actions(when);
+			},
+			becomes: function(v) {
+				callback(function(newValues,oldValues){
+					return newValues[name] === v && newValues[name] !== oldValues[name];
+				});
+				return actions(when);
+			},
+			updates: function () {
+				callback(function(){
+					return true;
+				});
+				return actions(when);
+			}
+		};
+	} else {
+		return {
 			remove: function () {
 				when.remove();
 			},
-            verify: function(f) {
-                callback(function(newValues,oldValues){
-                    var newArgs = [];
-                    var oldArgs = [];
-                    names.forEach(function(name) {
-                        newArgs.push(newValues[name]);
-                        oldArgs.push(oldValues[name]);
-                    });
-                    return f.apply(null, newArgs.concat(oldArgs));
-                });
-                return actions(when);
-            }
-        }
-    }
+			verify: function(f) {
+				callback(function(newValues,oldValues){
+					var newArgs = [];
+					var oldArgs = [];
+					names.forEach(function(name) {
+						newArgs.push(newValues[name]);
+						oldArgs.push(oldValues[name]);
+					});
+					return f.apply(null, newArgs.concat(oldArgs));
+				});
+				return actions(when);
+			}
+		}
+	}
 
 };
